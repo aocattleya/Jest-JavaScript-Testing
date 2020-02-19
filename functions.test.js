00884960379270b73f6d5990842dca4d454bcc48
aocattleya <aocattleya@yahoo.co.jp>
2020-02-19 13:07:54 +0900

    add: nullを判定するテスト

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,7 @@ test("adds 2+ 2 to equal 4", () => {
 test("adds 2+ 2 to equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+test("aShould be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
