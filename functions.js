6fa624ead55756f205bffa02c365d9c9e45bd0c0
aocattleya <aocattleya@yahoo.co.jp>
2020-02-19 13:22:12 +0900

    add: オブジェクトの

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
-  isNull: () => null
+  isNull: () => null,
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: "cattleya" };
+    user["lastName"] = "ao";
+    return user;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,13 +1,29 @@
 const functions = require("./functions");
 
+// toBe
 test("adds 2+ 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// not
 test("adds 2+ 2 to equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
-test("aShould be null", () => {
+// toBeNull
+test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test("User should be ao cattleya", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "cattleya",
+    lastName: "ao"
+  });
 });
